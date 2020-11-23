b93445fb16fefe981407c5b96ad712be58352d50
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-11-22 21:44:33 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+let button = document.getElementById("submit")
+
 document.addEventListener("DOMContentLoaded", () => {
+  button.addEventListener("click", function(event) { 
+    const task = document.getElementById("new-task-description").value;
+
+    let ul = document.getElementById("tasks");
+    let li = document.createElement("li");
+
+    ul.append(task, li);
+
+    event.target.removeEventListener;
+    event.preventDefault();
+  }, false);
   // your code here
 });
